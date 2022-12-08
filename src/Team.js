03741ed8bb97d6d7cef968b835ae32036b3cd49b
Shin Sergey <shin.sergey.t@gmail.com>
2022-12-09 01:49:32 +0600

    09.12

--- src/Team.js
+++ src/Team.js
@@ -1,12 +1,12 @@
 export default class Team {
-  * [Symbol.iterator]() {
-    let current = Object.keys(this)[0];
-    const size = Object.keys(this).length;
-    if (current <= size) {
-      yield current += 1;
-    }
-    return {
-      done: true,
+  constructor() {
+    *[Symbol.iterator]() {
+      const keys = Object.keys(this)
+      const size = keys.length;
+      let counter = 0
+      if (counter <= size) {
+      yield [keys[counter++]]
+      }
     };
   }
 }
